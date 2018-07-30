8e146816d4a51ada78fff302c96c77ae69a80c90
Amel Ajdinović <amel.a.z@gmail.com>
2018-07-30 12:09:47 +0200

    Updated example app

--- examples/example_app.rb
+++ examples/example_app.rb
@@ -5,15 +5,15 @@ compute = Fog::Compute::OneAndOne.new({
 })
 
 # Create SSH Key
-my_ssh_key = compute.ssh_keys.create(name: 'Example App Ssh Key',
+my_ssh_key = compute.ssh_keys.create(name: 'Fog Example App Ssh Key',
                                      description: 'My Fog SSH Key Description',
-                                     public_key: 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCezYMOgAan+JmatgFJ+Q1FUNjrqNVgAvcTkjYJwHVcQaolq9f9qB7tEeUPDNj2oNN49joAmTcllDcPIxryT5PnQUaaUhu4ZJ9+bRtXCyhnf2LJQdVfzBFEBJX9fW4RiV1XtSAtLRBrbrCb4JjHmhIYpvhBHC29Ve+g64nhdvBhqyLZ3SLI2U/opEmt5u2xftWGl0TBSQYveqc4ntz3fe+f9XlBHvK3Nw12bCLmLle7jQuZ4lXyAYqNAfdOMTs2zMTk422Dl/h4+zRh1h4rM9zaCk4+g3kdugJm7Vul03wm43cHmHsJv51R3XKSHzgb7q/eNj+YdMi5Ndt0Bm+bLjw4')
+                                     public_key: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCezYMOgAan+JmatgFJ+Q1FUNjrqNVgAvcTkjYJwHVcQaolq9f9qB7tEeUPDNj2oNN49joAmTcllDcPIxryT5PnQUaaUhu4ZJ9+bRtXCyhnf2LJQdVfzBFEBJX9fW4RiV1XtSAtLRBrbrCb4JjHmhIYpvhBHC29Ve+g64nhdvBhqyLZ3SLI2U/opEmt5u2xftWGl0TBSQYveqc4ntz3fe+f9XlBHvK3Nw12bCLmLle7jQuZ4lXyAYqNAfdOMTs2zMTk422Dl/h4+zRh1h4rM9zaCk4+g3kdugJm7Vul03wm43cHmHsJv51R3XKSHzgb7q/eNj+YdMi5Ndt0Bm+bLjw4")
 puts 'Creating ssh key...'
 
 # Create Server
-my_server = compute.servers.create(name: 'Example App Server',
-  fixed_instance_id: '6E1F2C70CCD3EE44ED194F4FFC47C4C9',
-  appliance_id: '8E3BAA98E3DFD37857810E0288DD8FBA',
+my_server = compute.servers.create(name: 'Fog Example App Server',
+  fixed_instance_id: 'CCC1C54896223068E05EFC17614EE690',
+  appliance_id: 'C572494C271ED64142E5FD93E084BBFC',
   datacenter_id: '4EFAD5836CE43ACA502FD5B99BEE44EF',
   public_key: [my_ssh_key.id])
 
@@ -38,7 +38,7 @@ rule2 = {
 
 rules = [rule1, rule2]
 
-my_firewall = compute.firewalls.create(name: 'Example App Firewall',
+my_firewall = compute.firewalls.create(name: 'Fog Example App Firewall',
   rules: rules)
 puts 'Creating Firewall...'
 puts my_firewall.wait_for { ready? }
@@ -55,7 +55,7 @@ rule1 = {
 
 rules = [rule1]
 
-my_load_balancer = compute.load_balancers.create(name: 'Example App LB',
+my_load_balancer = compute.load_balancers.create(name: 'Fog Example App LB',
   rules: rules, health_check_test: 'TCP', health_check_interval: 40,
   persistence: true, persistence_time: 1200, method: 'ROUND_ROBIN',
   datacenter_id: '4EFAD5836CE43ACA502FD5B99BEE44EF')
@@ -80,7 +80,7 @@ puts my_server.wait_for { ready? }
 
 # Create Block Storage
 my_block_storage = compute.block_storages.create(
-  name: 'My Block Storage', size: 20,
+  name: 'My Fog Block Storage', size: 20,
   description: 'Testing fog block storage',
   datacenter_id: '4EFAD5836CE43ACA502FD5B99BEE44EF',
   server_id: my_server.id
@@ -98,6 +98,7 @@ puts 'Destroying load balancer...'
 puts my_load_balancer.destroy
 
 puts "----------"
+puts my_firewall.wait_for { ready? }
 puts 'Destroying firewall...'
 puts my_firewall.destroy
 
